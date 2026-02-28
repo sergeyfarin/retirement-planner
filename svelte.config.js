14fb0eb7b931e36f9b705eb9d4531763188724e4
Sergey Farin <1467219+sergeyfarin@users.noreply.github.com>
2026-02-28 14:02:16 +0100

    feat: switch to @sveltejs/adapter-static for improved deployment compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		// Use the static adapter for a single-page frontend-only app.
+		// Provide a fallback to support client-side routing in SPA mode.
+		adapter: adapter({ fallback: 'index.html' }),
 		paths: {
       		base: '/retirement',
       		relative: false
