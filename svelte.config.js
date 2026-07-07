6a7d1dbb0b1eef9d4e983e2a717b1a74b7bac449
Sergey Farin <1467219+sergeyfarin@users.noreply.github.com>
2026-07-07 20:35:57 +0000

    updated packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const base = process.env.VITEST === 'true' ? '' : '/retirement';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		// Use the static adapter for a single-page frontend-only app.
 		// Provide a fallback to support client-side routing in SPA mode.
 		adapter: adapter({ fallback: 'index.html' }),
 		paths: {
-      		base: '/retirement',
-      		relative: false
-    	}
+			base,
+			relative: false
+		}
 	}
 };
 
